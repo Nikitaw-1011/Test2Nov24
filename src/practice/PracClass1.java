1bf3d2d7970e9e8f285d5482356824bc17e7b516
Nikitaw-1011 <nikitawani29@gmail.com>
2024-12-31 00:43:50 +0530

    Files chnages in branch A 1 file

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -3,7 +3,7 @@ package practice;
 public class PracClass1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello_Class1");
+		System.out.println("Hello_Class1, on branch A changes");
 
 	}
 
